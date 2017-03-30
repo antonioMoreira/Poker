4f83acde27ef739bfbe4a911ff09c2b3f2695e1b
murthaa <aka.murtha@gmail.com>
2017-03-30 11:43:07 -0300

    Add files via upload

---
+++ poker/src/Poker/Baralho.java
@@ -0,0 +1,39 @@
+package Poker;
+
+import java.util.Stack;
+import java.util.Collections;
+
+public class Baralho {
+	Stack<Carta> baralho = new Stack<Carta>();
+	Stack<Carta> descarte = new Stack<Carta>();
+	Carta carta;		
+	
+	/**
+	 * Construi um baralho ordenado e depois o embaralha 
+	 */
+	public Baralho(){
+		for(int i = 0 ; i < 4 ; i++){
+			for(int j = 0 ; j < 13 ; j++){
+				carta = new Carta(j,i);
+				baralho.push(carta);
+			}
+		}
+		Collections.shuffle(baralho);
+	}
+	
+	/**
+	 * Método retira uma carta do topo do baralho 
+	 */
+	public Carta retirarBaralho(){
+		return baralho.pop();
+	}
+	
+	/**
+	 *  Método recebe uma carta da mesa e a coloca na pilha de descarte
+	 * @param carta
+	 */
+	public void descarteCarta(Carta carta){
+		descarte.push(carta);
+	}
+	
+}
