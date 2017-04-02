b57adcbd0b86894949fb1c3531c372d5c36a5487
murthaa <aka.murtha@gmail.com>
2017-04-02 20:19:42 -0300

    Add files via upload

--- poker/src/Poker/Carta.java
+++ poker/src/Poker/Carta.java
@@ -13,7 +13,7 @@ public class Carta {
 	 * @param nipe
 	 * 		Um inteiro que representa o naipe da carta. 0 → copas; 1 → paus; 2 → ouro; 3 → espada 
 	 */
-	public Carta(int valorCarta, char naipe){
+	public Carta(int valorCarta, int naipe){
 		this.valorCarta = valorCarta;
 		this.naipe = naipe;		
 	}
@@ -25,12 +25,167 @@ public class Carta {
 	public int getNaipe(){
 		return this.naipe;
 	}
-
 	public String toString(){
+		String result;
 		
+		result = new String();
+		char c = 0;
 		
+		switch(this.naipe){
+			case 0: /* Copas */
+				
+				
+				for(int i = 0 ; i < 13 ;i++){
+					int j = i + 50;
+					
+					if(i < 9 && i == valorCarta){
+						c = (char) (j);
+					}else{
+						switch(i){
+							case 9:
+								c = 'J';
+							break;
+							case 10:
+								c = 'Q';
+							break;
+							case 11:
+								c = 'K';
+							break;
+							case 12:
+								c = 'A';
+							break;
+						
+							
+						}
+					}
+					result = "*--------*\n";
+					if(c!=58)result += "|♥      " + c +"|\n";
+					else result += "|♥     " + 10 +"|\n";
+					result += "|        |\n" + "|        |\n" + "|        |\n";
+					if(c!=58) result += "|" + c + "      ♥|\n";
+					else result += "|" + 10 + "     ♥|\n";
+					result += "*--------*\n";
+					if(c !=0) break;
+					else result = "";
+				}
+			return result;
+				
+			case 1: /* Paus */
+				
+	
+				for(int i = 0 ; i < 13 ;i++){
+					int j = i + 50;
+					if(i < 9 && i == valorCarta){
+						c = (char) (j);
+					}else{
+						switch(i){
+							case 9:
+								c = 'J';
+							break;
+							case 10:
+								c = 'Q';
+							break;
+							case 11:
+								c = 'K';
+							break;
+							case 12:
+								c = 'A';
+							break;
+							
+							
+						}
+						
+					}
+					
+					result = "*--------*\n";
+					if(c!=58)result += "|♣      " + c +"|\n";
+					else result += "|♣     " + 10 +"|\n";
+					result += "|        |\n" + "|        |\n" + "|        |\n";
+					if(c!=58) result += "|" + c + "      ♣|\n";
+					else result += "|" + 10 + "     ♣|\n";
+					result += "*--------*\n";
+					if(c != 0) break;
+					else result = "";
+				}
+			return result;
+			
+			case 2: /* Ouro */
+				
+	
+				for(int i = 0 ; i < 13 ;i++){
+					int j = i + 50;
+					if(i < 9 && i == valorCarta){
+						c = (char) (j);
+					}else{
+						switch(i){
+							case 9:
+								c = 'J';
+							break;
+							case 10:
+								c = 'Q';
+							break;
+							case 11:
+								c = 'K';
+							break;
+							case 12:
+								c = 'A';
+							break;
+							
+						}
+						
+					}
+					
+					result = "*--------*\n";
+					if(c!=58)result += "|♦      " + c +"|\n";
+					else result += "|♦    " + 10 +"|\n";
+					result += "|        |\n" + "|        |\n" + "|        |\n";
+					if(c!=58)result += "|" + c + "      ♦|\n";
+					else result += "|" + 10 + "    ♦|\n";
+					result += "*--------*\n";
+					if(c !=0) break;
+					else result = "";
+				}
+			return result;
+			
+			case 3: /* Espada */
+				result = "*--------*\n";
+	
+				for(int i = 0 ; i < 13 ;i++){
+					int j = i + 50;
+					if(i < 9 && i == valorCarta){
+						c = (char) (j);
+					}else{
+						switch(i){
+							case 9:
+								c = 'J';
+							break;
+							case 10:
+								c = 'Q';
+							break;
+							case 11:
+								c = 'K';
+							break;
+							case 12:
+								c = 'A';
+							break;
+						}
+						
+					}
+					
+					result = "*--------*\n";
+					if(c!=58)result += "|♠      " + c + "|\n";
+					else result += "|♠     " + 10 + "|\n";
+					result += "|        |\n" + "|        |\n" + "|        |\n";
+					if(c!=58) result += "|" + c + "      ♠|\n";
+					else result += "|" + 10 + "     ♠|\n";
+					result += "*--------*\n";
+					if(c !=0) break;
+					else result = "";
+				}
+			return result;				
+		}
 		
-		return null;
-	}	
+		return result;	
+	}
 	
 }
